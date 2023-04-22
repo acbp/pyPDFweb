e8fc61cc16e6fb350a7c5f771dc327c2833e45bc
Alexandre Calil Blasizza Paravani <acbp@users.noreply.github.com>
2023-04-22 18:42:08 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,6 +9,7 @@ mkdir -p $STORAGE_DIR/chrome
 cd $STORAGE_DIR/chrome
 wget -P ./ https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 dpkg -x ./google-chrome-stable_current_amd64.deb $STORAGE_DIR/chrome
+ln -sr /usr/bin/chrome
 rm ./google-chrome-stable_current_amd64.deb
 cd $HOME/project/src # Make sure we return to where we were
 echo "Installed Chrome"
