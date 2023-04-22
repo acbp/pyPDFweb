f2a5cec6c694eec8711d3995743d3bb39fd82723
Alexandre Calil Blasizza Paravani <alexandre.paravani@gruponewway.com.br>
2023-04-22 20:24:17 -0300

    fixed

--- setup.sh
+++ setup.sh
@@ -2,11 +2,23 @@
 #exit on error
 set -o errexit
 
-STORAGE_DIR=./
+echo "Instalando dependencias do chrome"
+apt-get update 
+apt-get install -y libglib2.0-0\
+    libnss3\
+    libgconf-2-4\
+    libfontconfig1
+
 echo "…Downloading Chrome"
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
-sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
-apt-get -y update
-apt-get install -y google-chrome-stable
+
+wget -P ./ https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+dpkg -x ./google-chrome-stable_current_amd64.deb ./chrome
+
+ln -sr ./chrome /usr/bin/google-chrome-stable
+ln -sr ./chrome /usr/bin/google-chrome
+ln -sr ./chrome /usr/local/bin/google-chrome
+ln -sr ./chrome /usr/local/bin/google-chrome-stable
+
+rm ./google-chrome-stable_current_amd64.deb
 
 pip install -r requirements.txt;
--- test.py
+++ test.py
@@ -29,14 +29,16 @@ def clique( target ):
 
 # operação headless ( sem abrir navegador )
 chrome_options = Options()
-chrome_options.add_argument("--headless=new")
 chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-gpu")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--headless=new")
+chrome_options.add_argument('--remote-debugging-port=9222')
 
 # Create a Chrome driver instance
 # driver = webdriver.Chrome(chrome_options)
 driver = webdriver.Chrome(
-    service=ChromeService(ChromeDriverManager().install())
-    , options=chrome_options
+    options=chrome_options
 )
 
 # foco da janela principal
