4b0e8243e24b17c7e8fd537d218bf9cdf7973bb4
Alexandre Calil Blasizza Paravani <acbp@users.noreply.github.com>
2023-04-22 18:27:29 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,26 +1,22 @@
 #!/usr/bin/env bash
-# exit on error
+#exit on error
 set -o errexit
 
 STORAGE_DIR=/opt/render/project/.render
-
 if [[ ! -d $STORAGE_DIR/chrome ]]; then
-echo "...Downloading Chrome"
+echo "…Downloading Chrome"
 mkdir -p $STORAGE_DIR/chrome
 cd $STORAGE_DIR/chrome
-wget -P ./ https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-apt-get install -y libglib2.0-0=2.50.3-2 \
-libnss3=2:3.26.2-1.1+deb9u1 \
-libgconf-2-4=3.2.6-4+b1 \
-libfontconfig1=2.11.0-6.7+b1 \
-chromium-browser
-sudo dpkg -x ./google-chrome-stable_current_amd64.deb $STORAGE_DIR/chrome
+wget -P ./ https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb 3
+dpkg -x ./google-chrome-stable_current_amd64.deb $STORAGE_DIR/chrome
 rm ./google-chrome-stable_current_amd64.deb
 cd $HOME/project/src # Make sure we return to where we were
-echo "Chrome at $STORAGE_DIR/chrome"
+echo "Installed Chrome"
 else
-echo "...Using Chrome from cache"
+echo "…Using Chrome from cache"
 fi
 
-pip install --upgrade pip
-pip install -r requirements.txt
+export PATH="${PATH}:/opt/render/project/.render/chrome/opt/google/chrome/google-chrome/chromedriver";
+export CHROME_PATH="/opt/render/project/.render/chrome/opt/google/chrome/google-chrome/chromedriver";
+
+pip install -r requirements.txt;
